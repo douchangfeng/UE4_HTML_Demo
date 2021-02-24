db28a07d001fd11f3e5282e4eb5bfd03e5f57c67
douchangfeng <1326737369@qq.com>
2021-02-24 11:00:43 +0800

    UE4与HTML双向通信 HTML调用UE4蓝图代码

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # UE4_HTML_Demo
 UE4与HTML双向通信Demo
+
+需要将 cef_binary64位 文件夹中的文件替换掉 Engine 文件
+1.UnrealEngine-4.26.1-release\Engine\Binaries\ThirdParty\CEF3\Win64 对应的文件全部替换
+2.UnrealEngine-4.26.1-release\Engine\Source\ThirdParty\CEF3\cef_binary_3.3071.1611.g4a19305_windows64只替换libcef.lib
---
+++ Source/HTMLDemo/HTMLDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HTMLDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HTMLDEMO_API AHTMLDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
